e49949f9d61e444e29102a1192860456442252e2
IlhamKurniawanBlora <ilhamkurniawanjateng@gmail.com>
2025-06-12 04:32:18 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
-    // Private keys (hanya tersedia di server-side)
-    // eslint-disable-next-line node/prefer-global/process
     supabaseServiceRole: process.env.SUPABASE_SERVICE_ROLE,
     // Public keys (tersedia di client-side)
     public: {
-      // eslint-disable-next-line node/prefer-global/process
       baseUrl: process.env.BASE_URL || 'http://localhost:3000',
-      // eslint-disable-next-line node/prefer-global/process
       supabaseUrl: process.env.SUPABASE_URL,
-      // eslint-disable-next-line node/prefer-global/process
       supabaseKey: process.env.SUPABASE_KEY,
     },
   },
